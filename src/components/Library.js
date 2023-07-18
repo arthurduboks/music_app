ae04d56ea08755693fb7325dcae0dcbdef4b5da4
Arthur Duboks <duboks.arthur@gmail.com>
2023-07-17 21:18:06 -0400

    first commit

---
+++ src/components/Library.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LibrarySong from "./LibrarySong";
+
+const Library = ({
+  songs,
+  setCurentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
+  return (
+    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
+      <h2>Library</h2>
+      <div className="library-songs">
+        {songs.map((song) => (
+          <LibrarySong
+            song={song}
+            songs={songs}
+            setCurentSong={setCurentSong}
+            id={song.id}
+            key={song.id}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+            setSongs={setSongs}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const LibrarySong = ({
+  song,
+  songs,
+  setCurentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandle = async () => {
+    await setCurentSong(song);
+    audioRef.current.play();
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div
+      onClick={songSelectHandle}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img src={song.cover} alt={song.name} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
